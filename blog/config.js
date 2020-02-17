a2c6385bd5329e8e825c4fe4c0ed618a9e78577b
Christian F. Jung <christianfjung@gmail.com>
2020-02-17 15:13:32 -0500

    xox

---
+++ blog/config.js
@@ -0,0 +1,53 @@
+'use strict';
+
+module.exports = {
+  url: 'https://blog.christianfjung.com',
+  pathPrefix: '/',
+  title: 'Blog by Christian F Jung',
+  subtitle: '',
+  copyright: '',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: '',
+  useKatex: false,
+  menu: [
+    {
+      label: 'Articles',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    },
+    // {
+    //   label: 'Contact me',
+    //   path: '/pages/contacts'
+    // },
+    // {
+    //   label: 'Home',
+    //   link: 'google.com' }
+
+  ],
+  author: {
+    name: 'Christian F. Jung',
+    photo: '/photo.jpg',
+    bio: '2nd year student at UVA interested in data and computer science.',
+    contacts: {
+      email: 'cfj@christianfjung.com',
+      facebook: '',
+      telegram: '',
+      twitter: 'christianfjung',
+      github: 'christianfjung',
+      rss: '',
+      vkontakte: '',
+      linkedin: 'christianfjung',
+      instagram: '',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: '',
+      youtube: '',
+      soundcloud: '',
+    }
+  }
+};
---
+++ blog/content/pages/about.md
@@ -0,0 +1,11 @@
+---
+title: "About me"
+template: "page"
+socialImage: "/media/photo.jpg"
+---
+
+I'm a second year student studying computer science at the University of Virginia, interested in the intersection of data and people. I currently teach an introductory data science class at [HackCville](https://hackcville.com/course/node/) and have joined clubs that have allowed me to explore computer science in an interdisciplinary fashion. This diversity will allow me to be cognizant of biases when I develop algorithms and analyze data.
+
+On a personal note, I'm the oldest of 4 siblings, I love to run, scuba dive, and volunteer with children in STEM!
+
+If you are looking for interns, check out my [resume.](http://christianfjung.com/resume.pdf) Or if you just want to reach out, please don’t hesitate to contact me!
